2f00b527a85fd560305db2cccd9ffb870bd9aeac
Amos Gloria <amoskesegloria@gmail.com>
2025-09-19 23:26:05 +0100

    added files to be worked on

---
+++ index.js
@@ -0,0 +1,22 @@
+const inventory = [];
+
+
+// Function to add a Product to the inventory by Mariam Mussa : mariamkilangi@outlook.com
+
+
+
+// Function to remove product by Nakayiza Kevina : nakayizakevina@outlook.com
+
+
+
+// Function to update stock by Nancy Ndungu : nyokabi5991@outlook.com
+
+
+
+
+// Function to generate report by Olaleye Oluwatomide Paulina : paulinaarike@outlook.com
+
+
+
+
+// Function to search products by Amos Gloria : amoskesegloria@outlook.com
